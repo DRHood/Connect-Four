932c9df2d54b67fbb7a77543d51975fb52fcb02e
DRHood <darryl.r.hood@gmail.com>
2020-02-10 14:35:02 -0500

    developing game logic, reorganized html grid elements

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+const startB = document.querySelector('#s-btn')
+const boardCells = document.querySelectorAll('.cell')
+
+
+let boardGrid = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
+
+startB.addEventListener('click', start);
+
+function pickMove() {
+    for (let i = 0; i < boardCells.length; i++) {
+        let bCell = boardCells[i];
+        bCell.addEventListener('click', function() {
+         console.log('clicked');
+        });
+      }
+}
+
 function start() {
-    
+    pickMove();
 }
