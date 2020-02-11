491fe3e22e3c4337a53f72458ff508cdf5a8c9bc
DRHood <darryl.r.hood@gmail.com>
2020-02-11 05:21:53 -0500

    working out game logic

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-const startB = document.querySelector('#s-btn')
+/* global variables to use in functions */
+const startB = document.querySelector('#s-btn'); //start button
 const boardCells = document.querySelectorAll('.cell')
+let gameActive = false; // used to prevent dropping cells once game is over
+let activePlayer = 0; // # of active player - 1 or 2.  Default 0, no active player
+let playerColor = []; //playerColor array
+playerColor[1] = "red"; //player 1 "red" 
+playerColor[2] = "yellow"; //player 2 "yellow"
 
-
-let boardGrid = [
+// let gameBoard = []; // gameBoard as empty array
+let gameBoard = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -13,24 +19,13 @@ let boardGrid = [
 
 startB.addEventListener('click', start);
 
-function pickMove() {
-    for (let i = 0; i < boardCells.length; i++) {
-        let bCell = boardCells[i];
-        bCell.addEventListener('click', function() {
-         console.log('clicked');
-        });
-      }
-}
-
 function start() {
-    pickMove();
-}
-
-
-
-
-// setting falling animation for piece
-setTimeout(() => {
-    document.getElementById('foo').classList.add('fall');
-  }, 1000);
-  
+    if (gameActive == true) return false; 
+    gameActive = true;
+    for (row = 0; row <= 5; row++) {
+        gameBoard[row] = [];
+        for (col = 0; col <= 6; col++) {
+            gameBoard[row][col] = 0;
+        }	
+    }		      
+   
